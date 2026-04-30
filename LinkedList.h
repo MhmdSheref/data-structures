7dbdff40dba0b3fc16ee45e375a896568f53da98
Noureldin Islam <noureldinislam2006@gmail.com>
2026-04-30 23:22:34 +0300

    update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "Node.h"
+class LinkedList {
+private:
+	Node* head;
+	Node* tail;
+	int length;
+public:
+	LinkedList(Node* h);
+	void append(int value);
+	void prepend(int value);
+	int removeLast();
+	int removeFirst();
+	void Display();
+};
+
+#endif // !LINKEDLIST_H
+
+
